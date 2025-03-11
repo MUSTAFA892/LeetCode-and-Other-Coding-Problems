71b6d779e173a147c648d122a75e6b52519b446d
MUSTAFA892 <mustafatinwala6@gmail.com>
2025-03-11 15:44:20 +0530

    s

--- 209. Minimum Size Subarray Sum/main.py
+++ 209. Minimum Size Subarray Sum/main.py
@@ -17,3 +17,26 @@ class Solution:
         if min_len < float('inf'):
             return min_len
         return 0
+
+"""        ans = []
+
+        left = 0
+        right = 0
+
+        while right < len(nums):
+            sum_ = sum(nums[left:right+1])
+            if sum_ == target:
+                ans.append(nums[left:right+1])
+                right += 1
+            elif sum_ > target:
+                left += 1
+            else:
+                right +=1 
+
+        len_ = []
+        for i in ans:
+            len_.append(len(i))
+        if len_:
+            return min(len_)
+        else:
+            return 0"""
