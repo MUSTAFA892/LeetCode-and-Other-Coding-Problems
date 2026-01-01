9fda10c3a8c725da47bdebbc19f4a9c1e55fd853
MUSTAFA892 <mustafatinwala6@gmail.com>
2026-01-01 12:45:33 +0530

    updated the leetcode problem no. 43

--- 43. Multiply Strings/main.py
+++ 43. Multiply Strings/main.py
@@ -1,4 +1,18 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
+        mul_val = []
+
+        for i in range(len(num2)):
+            zeros = (len(num2)-1)-i
+            mul_val.append(str(int(num2[i])*int(num1))+'0'*zeros)
+            
+        product_val = 0
+        for i in mul_val:
+            product_val += int(i)
+
+        return str(product_val)
+    
+"""class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
         mul = int(num1) * int(num2)
-        return str(mul)
+        return str(mul)"""
